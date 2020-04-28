7261eca894eca9b19f80a32389b44ad8d2298907
Yeismer Espejo <yespejo@minka.io>
2020-04-27 23:13:52 -0500

    set enviroment database

--- src/database.js
+++ src/database.js
@@ -3,24 +3,13 @@ const { promisify } = require('util');
 const AWS = require("aws-sdk");
 const fs = require('fs');
 
-/*const database={
+const database={
     host:process.env.HOST,
     port:process.env.PORT_DB,
     user:process.env.USER_DB,
     password:process.env.PASSWORD_DB,
     database:process.env.DATABASE
 };
-*/
-
-const database={
-    host:'smarttools.czga9dhbndmp.us-east-2.rds.amazonaws.com',
-    port:'4543',
-    user:'admin',
-    password:'admin123456',
-    database:'smarttools'
-};
-
-
 
 console.log(database);
 
