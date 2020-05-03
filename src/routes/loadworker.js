2a4d16a79fd9b0d9807872f6d2d6dea181744529
Yeismer Espejo <yespejo@minka.io>
2020-05-02 19:28:24 -0500

    Update loadworker.js

--- src/routes/loadworker.js
+++ src/routes/loadworker.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) =>{
+router.get('/loadworker', (req, res) =>{
     console.log("hecho");
     res.render('index');
 });
